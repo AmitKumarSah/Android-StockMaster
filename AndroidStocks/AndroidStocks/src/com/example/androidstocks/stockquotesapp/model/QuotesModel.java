a678c142d8bea6baad4eb791e0879cef9b03f7f3
Rishi <coder.deftsoft@gmail.com>
2013-02-18 17:11:46 +0530

    Commited

---
+++ AndroidStocks/AndroidStocks/src/com/example/androidstocks/stockquotesapp/model/QuotesModel.java
@@ -0,0 +1,44 @@
+package com.example.androidstocks.stockquotesapp.model;
+
+import java.util.List;
+
+import com.example.androidstocks.stockquotesapp.vo.QuoteVO;
+
+
+
+public class QuotesModel {
+
+	private static QuotesModel instance;
+
+	private List<QuoteVO> quotes;
+
+	private String[] quotsData;
+
+	private QuotesModel() {
+
+	}
+
+	public static synchronized QuotesModel getInstance() {
+		if (instance == null)
+			instance = new QuotesModel();
+
+		return instance;
+	}
+
+	public List<QuoteVO> getQuotes() {
+		return quotes;
+	}
+
+	public void setQuotes(List<QuoteVO> quotes) {
+		this.quotes = quotes;
+	}
+
+	public String[] getQuotsData() {
+		return quotsData;
+	}
+
+	public void setQuotsData(String[] quotsData) {
+		this.quotsData = quotsData;
+	}
+
+}
